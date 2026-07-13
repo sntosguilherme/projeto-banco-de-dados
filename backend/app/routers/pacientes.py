704ab4a3c286e1a5d991df706a3f8f2aace704d1
rebecabelmont <rebeca.belmont777@gmail.com>
2026-07-12 23:55:05 -0300

    feat: adicionando o arquivo do modelo relacional

--- backend/app/routers/pacientes.py
+++ backend/app/routers/pacientes.py
@@ -29,10 +29,9 @@ def atualizar_paciente(id_paciente: int, dados: PacienteUpdate):
         with get_db_connection() as conn:
             with conn: 
                 with conn.cursor() as cursor:
-            with conn.cursor() as cursor:
-                if dados.num_convenio is not None:
-                    sql = load_query(ARQUIVO_SQL, "atualizar_paciente_convenio")
-                    cursor.execute(sql, (dados.num_convenio, id_paciente))
+                    if dados.num_convenio is not None:
+                        sql = load_query(ARQUIVO_SQL, "atualizar_paciente_convenio")
+                        cursor.execute(sql, (dados.num_convenio, id_paciente))
 
                 if dados.alergias is not None:
                     sql = load_query(ARQUIVO_SQL, "atualizar_paciente_alergias")
